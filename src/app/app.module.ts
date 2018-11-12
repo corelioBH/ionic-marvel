df529568fceabc020c646f1acac385f4f3188df3
corelioBH <corelio@gmail.com>
2018-11-12 01:38:21 -0200

    Exemplo simples de Ionic para invocacao ao projeto de API da Marvel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HeroService } from '../providers/hero-service/hero-service';
+import { HttpModule } from '@angular/http';
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,6 +19,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +30,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    HeroService
   ]
 })
-export class AppModule {}
+export class AppModule { }
